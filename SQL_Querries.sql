738e00d9d44f92927b594baa27a6d60002fe885d
AJ5831A <jakhararyan27@gmail.com>
2025-02-21 00:13:24 +0530

    Added some querries

---
+++ SQL_Querries.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM employee_demographics;
+
+SELECT first_name , 
+last_name ,
+age,
+age + 10
+FROM Parks_and_Recreation.employee_demographics;
+
+# PEDMAS order for operations in SQL
+
+SELECT DISTINCT gender
+FROM Parks_and_Recreation.employee_demographics;
